e69a7e483409dc83692b2446f869252b7d91a457
Paulo Avra <pauloavra@gmail.com>
2021-11-25 00:38:36 +0000

    feat: add crad for marcelo to test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,32 @@ const Homepage = () => {
         <button onClick={() => authenticate()}>login</button>
       )}
       <div>{wallet}</div>
+      <div className="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="bg-white overflow-hidden shadow rounded-lg">
+          <div className="p-5">
+            <div className="flex items-center">
+              <div className="flex-shrink-0"></div>
+              <div className="ml-5 w-0 flex-1">
+                <dl>
+                  <dt className="text-sm font-medium text-gray-500 truncate">name</dt>
+                  <dd>
+                    <div className="text-lg font-medium text-gray-900">amount</div>
+                  </dd>
+                </dl>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-5 py-3">
+            <button
+              type="button"
+              className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={() => console.log(123)}
+            >
+              Button text
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
