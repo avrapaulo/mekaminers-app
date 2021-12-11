cad512f6d1b611a3095e2e97b25c45c62b3a22e5
Paulo Avra <pauloavra@gmail.com>
2021-12-11 13:14:39 +0000

    fix: activity layout img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const Homepage = () => {
   return (
-    <div className="uppercase flex justify-center items-center h-screen -mt-16 lg:-mt-28 text-white font-bold">
+    <div className="uppercase flex justify-center items-center h-screen -mt-16 lg:-mt-28 text-white font-bold -z-10">
       <div className="md:relative md:p-80">
         <Image alt="Logo Meka Miners" layout="fill" objectFit="contain" src="/ops.png" />
       </div>
