d6855661dc604a727b2e9c22e23a1a57464e0c3f
Paulo Avra <pauloavra@gmail.com>
2021-12-20 00:26:13 +0000

    fix: state inventory

--- src/components/card/state.tsx
+++ src/components/card/state.tsx
@@ -10,7 +10,7 @@ export const State = ({ stateId }: StateProps) => (
     {stateId === 'selling' && (
       <div
         title="On sale"
-        className="absolute bg-green-600 bottom-2 -right-2.5 xl:-right-1 z-10 block rounded-full"
+        className="absolute bg-green-600 bottom-2 -right-3 z-10 block rounded-full"
       >
         <CurrencyDollarIcon className="h-7 w-7 text-black" aria-hidden="true" />
       </div>
@@ -18,7 +18,7 @@ export const State = ({ stateId }: StateProps) => (
     {stateId === 'farm' && (
       <div
         title="Farm"
-        className="absolute bg-black bottom-2 -right-2.5 xl:-right-1 z-10 block rounded-full ring-2 ring-pickled-bean-500"
+        className="absolute bg-black bottom-2 -right-3 z-10 block rounded-full ring-2 ring-pickled-bean-500"
       >
         <PickaxeIcon className="h-6 w-6 p-1.5 text-pickled-bean-300" aria-hidden="true" />
       </div>
