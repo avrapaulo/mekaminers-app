c4ec494ac4dc283af8f32678bdf10f6ac6e9fc7f
Paulo Avra <pauloavra@gmail.com>
2021-12-28 18:18:05 +0000

    feat: remove header boxes

--- src/components/inventory/layout.tsx
+++ src/components/inventory/layout.tsx
@@ -6,18 +6,6 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => (
   <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 text-white">
-    <Link href="/inventory/robots">
-      <a>robots</a>
-    </Link>
-    <Link href="/inventory/boxes">
-      <a> boxes</a>
-    </Link>
-    <Link href="/inventory/tools">
-      <a> tools</a>
-    </Link>
-    <Link href="/inventory/pieces">
-      <a> pieces</a>
-    </Link>
     <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-4 lg:gap-x-5 mt-10">
       {children}
     </div>
