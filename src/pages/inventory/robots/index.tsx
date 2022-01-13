8827a799efe765d84c07ead6d0eba125507daaf7
Paulo Avra <pauloavra@gmail.com>
2022-01-13 20:01:20 +0000

    feat: fake robots test mobile

--- src/pages/inventory/robots/index.tsx
+++ src/pages/inventory/robots/index.tsx
@@ -52,55 +52,267 @@ const RobotsPage = () => {
   return (
     <>
       <MiniHeader />
-      {data === null ? (
-        isLoadingPage || isLoading || isFetching ? (
-          <div className="flex h-full justify-center items-center animation-y">
-            <div className="h-40 w-40 relative">
-              <img alt="Logo Meka Miners" src={'/meka.png'} />
-            </div>
-          </div>
-        ) : (
-          <div className="uppercase flex justify-center items-center text-white font-bold -z-10">
-            <img alt="Logo Meka Miners" width="500" src="/empty.png" />
-          </div>
-        )
-      ) : (
-        <Layout>
-          <>
-            {(data as RobotsProps[])?.map(
-              ({
-                token,
-                title,
-                rarity = 'default',
-                robotStatus,
-                piecesStatus,
-                bonus,
-                mode,
-                type
-              }) => (
-                <Link key={token} href={`/inventory/robots/${token}`}>
-                  <a className="relative flex justify-center">
-                    <Mode modeId={mode} />
 
-                    <Card
+      <Layout>
+        <>
+          {[
+            {
+              owner: '0xba6df131a1a10f560a62e8c2564d38f66b4c927b',
+              token: 235,
+              title: 'Basic',
+              type: 'Basic',
+              rarity: 'E',
+              bonus: 13,
+              robotStatus: [
+                {
+                  key: 'Capacity',
+                  value: 28
+                },
+                {
+                  key: 'Stealthiness',
+                  value: 6
+                },
+                {
+                  key: 'Efficiency',
+                  value: 60
+                },
+                {
+                  key: 'Speed',
+                  value: 7
+                },
+                {
+                  key: 'OilDecrease',
+                  value: 0
+                }
+              ],
+              piecesStatus: [],
+              mode: 1
+            },
+            {
+              owner: '0xba6df131a1a10f560a62e8c2564d38f66b4c927b',
+              token: 237,
+              title: 'Stealth',
+              type: 'Stealth',
+              rarity: 'E',
+              bonus: 10,
+              robotStatus: [
+                {
+                  key: 'Capacity',
+                  value: 26
+                },
+                {
+                  key: 'Stealthiness',
+                  value: 8
+                },
+                {
+                  key: 'Efficiency',
+                  value: 60
+                },
+                {
+                  key: 'Speed',
+                  value: 5
+                },
+                {
+                  key: 'OilDecrease',
+                  value: 0
+                }
+              ],
+              piecesStatus: [],
+              mode: 1
+            },
+            {
+              owner: '0xba6df131a1a10f560a62e8c2564d38f66b4c927b',
+              token: 238,
+              title: 'Tank',
+              type: 'Tank',
+              rarity: 'B',
+              bonus: 42,
+              robotStatus: [
+                {
+                  key: 'Capacity',
+                  value: 22
+                },
+                {
+                  key: 'Stealthiness',
+                  value: 5
+                },
+                {
+                  key: 'Efficiency',
+                  value: 60
+                },
+                {
+                  key: 'Speed',
+                  value: 3
+                },
+                {
+                  key: 'OilDecrease',
+                  value: 0
+                }
+              ],
+              piecesStatus: [],
+              mode: 1
+            },
+            {
+              owner: '0xba6df131a1a10f560a62e8c2564d38f66b4c927b',
+              token: 239,
+              title: 'Basic',
+              type: 'Basic',
+              rarity: 'E',
+              bonus: 15,
+              robotStatus: [
+                {
+                  key: 'Capacity',
+                  value: 30
+                },
+                {
+                  key: 'Stealthiness',
+                  value: 6
+                },
+                {
+                  key: 'Efficiency',
+                  value: 60
+                },
+                {
+                  key: 'Speed',
+                  value: 7
+                },
+                {
+                  key: 'OilDecrease',
+                  value: 0
+                }
+              ],
+              piecesStatus: [],
+              mode: 1
+            },
+            {
+              owner: '0xba6df131a1a10f560a62e8c2564d38f66b4c927b',
+              token: 241,
+              title: 'Tank',
+              type: 'Tank',
+              rarity: 'E',
+              bonus: 16,
+              robotStatus: [
+                {
+                  key: 'Capacity',
+                  value: 20
+                },
+                {
+                  key: 'Stealthiness',
+                  value: 5
+                },
+                {
+                  key: 'Efficiency',
+                  value: 60
+                },
+                {
+                  key: 'Speed',
+                  value: 3
+                },
+                {
+                  key: 'OilDecrease',
+                  value: 0
+                }
+              ],
+              piecesStatus: [],
+              mode: 1
+            },
+            {
+              owner: '0xba6df131a1a10f560a62e8c2564d38f66b4c927b',
+              token: 242,
+              title: 'Tank',
+              type: 'Tank',
+              rarity: 'D',
+              bonus: 24,
+              robotStatus: [
+                {
+                  key: 'Capacity',
+                  value: 20
+                },
+                {
+                  key: 'Stealthiness',
+                  value: 5
+                },
+                {
+                  key: 'Efficiency',
+                  value: 60
+                },
+                {
+                  key: 'Speed',
+                  value: 3
+                },
+                {
+                  key: 'OilDecrease',
+                  value: 0
+                }
+              ],
+              piecesStatus: [],
+              mode: 1
+            },
+            {
+              owner: '0xba6df131a1a10f560a62e8c2564d38f66b4c927b',
+              token: 245,
+              title: 'Tank',
+              type: 'Tank',
+              rarity: 'E',
+              bonus: 14,
+              robotStatus: [
+                {
+                  key: 'Capacity',
+                  value: 20
+                },
+                {
+                  key: 'Stealthiness',
+                  value: 5
+                },
+                {
+                  key: 'Efficiency',
+                  value: 60
+                },
+                {
+                  key: 'Speed',
+                  value: 3
+                },
+                {
+                  key: 'OilDecrease',
+                  value: 0
+                }
+              ],
+              piecesStatus: [],
+              mode: 1
+            }
+          ]?.map(
+            ({
+              token,
+              title,
+              rarity = 'default',
+              robotStatus,
+              piecesStatus,
+              bonus,
+              mode,
+              type
+            }) => (
+              <Link key={token} href={`/inventory/robots/${token}`}>
+                <a className="relative flex justify-center">
+                  <Mode modeId={mode} />
+
+                  <Card
+                    rarity={rarity}
+                    description={title}
+                    imageCard={<Robot rarity={rarity} robotType={type.toLowerCase()} />}
+                  >
+                    <RobotBody
+                      bonus={bonus}
+                      piecesStatus={piecesStatus}
                       rarity={rarity}
-                      description={title}
-                      imageCard={<Robot rarity={rarity} robotType={type.toLowerCase()} />}
-                    >
-                      <RobotBody
-                        bonus={bonus}
-                        piecesStatus={piecesStatus}
-                        rarity={rarity}
-                        robotStatus={robotStatus}
-                      />
-                    </Card>
-                  </a>
-                </Link>
-              )
-            )}
-          </>
-        </Layout>
-      )}
+                      robotStatus={robotStatus}
+                    />
+                  </Card>
+                </a>
+              </Link>
+            )
+          )}
+        </>
+      </Layout>
     </>
   )
 }
