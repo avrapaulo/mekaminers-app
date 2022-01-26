9b7bb4881a0eb805bd61b7984921c4690a88c58f
Paulo Avra <pauloavra@gmail.com>
2022-01-26 13:36:59 +0000

    maintenance up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -46,7 +46,10 @@ const App = ({ Component, pageProps }: AppProps) => {
       >
         <RecoilRoot>
           <Layout>
-            <Component {...pageProps} />
+            {/* <Component {...pageProps} /> */}
+            <div className="uppercase flex justify-center items-center h-screen -mt-16 lg:-mt-28 text-white font-bold -z-10">
+              <img alt="" width="500" height="100" src="/ops.png" />
+            </div>
           </Layout>
         </RecoilRoot>
       </MoralisProvider>
