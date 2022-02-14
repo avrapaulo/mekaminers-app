83cb2a722465728cc8f4158e3e8ffec057590f85
Paulo Avra <pauloavra@gmail.com>
2022-02-14 19:30:21 +0000

    feat: add erro fallback component

--- src/components/3D/canvas-container/index.tsx
+++ src/components/3D/canvas-container/index.tsx
@@ -1,6 +1,7 @@
 import { Suspense, useRef } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Environment, OrbitControls } from '@react-three/drei'
+import { ErrorBoundary } from 'react-error-boundary'
 
 interface CanvasContainerProps {
   autoRotate?: boolean
@@ -8,24 +9,34 @@ interface CanvasContainerProps {
   children: JSX.Element
 }
 
+const ErrorFallback = () => {
+  return (
+    <div className="text-center text-white">
+      <p>Something went wrong</p>
+    </div>
+  )
+}
+
 export const CanvasContainer = ({ children, camera, autoRotate = true }: CanvasContainerProps) => {
   const ref = useRef()
 
   return (
-    <Canvas resize={{ scroll: false }} shadows dpr={[1, 2]} camera={{ ...camera }}>
-      <OrbitControls
-        enableZoom={false}
-        ref={ref}
-        autoRotate={autoRotate}
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={1}
-      />
-      <Suspense fallback={null}>
-        <ambientLight intensity={0.1} />
-        <Environment preset="city" />
-        <spotLight intensity={1} position={[5, 20, 20]} />
-        {children}
-      </Suspense>
-    </Canvas>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Canvas resize={{ scroll: false }} shadows dpr={[1, 2]} camera={{ ...camera }}>
+        <OrbitControls
+          enableZoom={false}
+          ref={ref}
+          autoRotate={autoRotate}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={1}
+        />
+        <Suspense fallback={null}>
+          <ambientLight intensity={0.1} />
+          <Environment preset="city" />
+          <spotLight intensity={1} position={[5, 20, 20]} />
+          {children}
+        </Suspense>
+      </Canvas>
+    </ErrorBoundary>
   )
 }
