49a4f12c7425e9c9d0b221de78cb424660570df4
Paulo Avra <pauloavra@gmail.com>
2022-02-23 13:14:40 +0000

    feat: GAME ON

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -49,13 +49,14 @@ const App = ({ Component, pageProps }: AppProps) => {
         <RecoilRoot>
           <Toaster position="top-right" reverseOrder={false} />
           <Layout>
-            {active ? (
+            <Component {...pageProps} />
+            {/* {active ? (
               <Component {...pageProps} />
             ) : (
               <div className="uppercase flex justify-center items-center h-screen -mt-16 lg:-mt-28 text-white font-bold -z-10">
                 <img alt="" width="500" height="100" src="/ops.png" />
               </div>
-            )}
+            )} */}
           </Layout>
         </RecoilRoot>
       </MoralisProvider>
